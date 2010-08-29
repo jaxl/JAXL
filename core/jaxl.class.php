fbc6b47390c86d751e59f61e1243b33bb0fdb1e1
Abhinav Singh <mailsforabhinav@gmail.com>
2010-08-29 16:19:33 -0700

    Added magic method __call inside JAXL class, all application codes should now call a methods inside implemented XEP's like ->JAXLxxxx('method', , , ...). Also added ->log method to be used by application codes, JAXLog::log() should not be called directly from application code. Updated XMPPSend methods to accept  instance. Added ->requires() method that does a similar job as jaxl_require() which now also expects  instance as one of the passed parameter.

--- core/jaxl.class.php
+++ core/jaxl.class.php
@@ -70,11 +70,11 @@
             if(!JAXLUtil::isWin() && JAXLUtil::pcntlEnabled()) {
                 pcntl_signal(SIGTERM, array($this, "shutdown"));
                 pcntl_signal(SIGINT, array($this, "shutdown"));
-                JAXLog::log("Registering shutdown for SIGH Terms ...", 0);
+                JAXLog::log("Registering shutdown for SIGH Terms ...", 0, $this);
             }
             
             if(JAXLUtil::sslEnabled()) {
-                JAXLog::log("Openssl enabled ...", 0);
+                JAXLog::log("Openssl enabled ...", 0, $this);
             }
             
             if($this->mode == "cli") {
@@ -89,26 +89,24 @@
             }
 
             // include service discovery XEP, recommended for every IM client
-            jaxl_require('JAXL0030', array(
+            jaxl_require('JAXL0030', $this, array(
                 'category'=>'client',
                 'type'=>'bot',
                 'name'=>JAXL_NAME,
-                'lang'=>'en',
-                'instance'=>$this
+                'lang'=>'en'
             ));
         }
         
         function shutdown($signal) {
-            global $jaxl;
-            JAXLog::log("Jaxl Shutting down ...", 0);
-            JAXLPlugin::execute('jaxl_pre_shutdown', $signal);
+            JAXLog::log("Jaxl Shutting down ...", 0, $this);
+            JAXLPlugin::execute('jaxl_pre_shutdown', $signal, $this);
             
-            if($jaxl->stream) XMPPSend::endStream();
-            $jaxl->stream = false;
+            if($this->stream) XMPPSend::endStream($this);
+            $this->stream = false;
         }
         
         function auth($type) {
-            return XMPPSend::startAuth($type);
+            return XMPPSend::startAuth($type, $this);
         }
         
         function setStatus($status=false, $show=false, $priority=false, $caps=false) {
@@ -116,30 +114,30 @@
             $child['status'] = ($status === false ? 'Online using Jaxl (Jabber XMPP Library in PHP)' : $status);
             $child['show'] = ($show === false ? 'chat' : $show);
             $child['priority'] = ($priority === false ? 1 : $priority);
-            if($caps) $child['payload'] = JAXL0115::getCaps();
+            if($caps) $child['payload'] = JAXL0115::getCaps($this->features);
             
-            return XMPPSend::presence(false, false, $child);
+            return XMPPSend::presence(false, false, $child, false, $this);
         }
         
         function subscribe($toJid) {
-            return XMPPSend::presence($toJid, false, false, 'subscribe');
+            return XMPPSend::presence($toJid, false, false, 'subscribe', $this);
         }
         
         function subscribed($toJid) {
-            return XMPPSend::presence($toJid, false, false, 'subscribed');
+            return XMPPSend::presence($toJid, false, false, 'subscribed', $this);
         }
         
         function unsubscribe($toJid) {
-            return XMPPSend::presence($toJid, false, false, 'unsubscribe');
+            return XMPPSend::presence($toJid, false, false, 'unsubscribe', $this);
         }
         
         function unsubscribed($toJid) {
-            return XMPPSend::presence($toJid, false, false, 'unsubscribed');
+            return XMPPSend::presence($toJid, false, false, 'unsubscribed', $this);
         }
         
         function getRosterList($callback=false) {
             $payload = '<query xmlns="jabber:iq:roster"/>';
-            return XMPPSend::iq("get", $payload, false, $this->jid, $callback);
+            return XMPPSend::iq("get", $payload, false, $this->jid, $callback, $this);
         }
         
         function addRoster($jid, $group, $name=false) {
@@ -150,7 +148,7 @@
             $payload .= '<group>'.$group.'</group>';
             $payload .= '</item>';
             $payload .= '</query>';
-            return XMPPSend::iq("set", $payload, false, $this->jid);
+            return XMPPSend::iq("set", $payload, false, $this->jid, false, $this);
         }
         
         function updateRoster($jid, $group, $name=false, $subscription=false) {
@@ -162,7 +160,7 @@
             $payload .= '<group>'.$group.'</group>';
             $payload .= '</item>';
             $payload .= '</query>';
-            return XMPPSend::iq("set", $payload, false, $this->jid);
+            return XMPPSend::iq("set", $payload, false, $this->jid, false, $this);
         }
         
         function deleteRoster($jid) {
@@ -170,13 +168,32 @@
             $payload .= '<item jid="'.$jid.'" subscription="remove">';
             $payload .= '</item>';
             $payload .= '</query>';
-            return XMPPSend::iq("set", $payload, false, $this->jid);
+            return XMPPSend::iq("set", $payload, false, $this->jid, false, $this);
         }
         
         function sendMessage($to, $message, $from=false, $type='chat') {
             $child = array();
             $child['body'] = $message;
-            return XMPPSend::message($to, $from, $child, $type);
+            return XMPPSend::message($to, $from, $child, $type, $this);
+        }
+
+        function log($log, $level) {
+            JAXLog::log($log, $level, $this);
+        }
+
+        function requires($class) {
+            jaxl_require($class, $this);
+        }
+
+        function __call($xep, $param) {
+            $param[] = $this;
+            $method = array_shift($param);
+            if(substr($xep, 0, 4) == 'JAXL') {
+                $xep = substr($xep, 4, 4);
+                if(is_numeric($xep)
+                && class_exists('JAXL'.$xep)
+                ) { call_user_func_array(array('JAXL'.$xep, $method), $param); }
+            }
         }
         
     }
