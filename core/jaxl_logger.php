2abb54b743fd299bdd862a6b14d7d1175fd39538
Abhinav Singh <mailsforabhinav@gmail.com>
2012-10-04 10:37:27 +0530

    logger will now truncate msg to 1000 max length due to error_log 1024 max size limit (log_errors_max_len ini setting need to be checked as an alternative)

--- core/jaxl_logger.php
+++ core/jaxl_logger.php
@@ -60,6 +60,7 @@ class JAXLLogger {
 	
 	public static $level = JAXL_DEBUG;
 	public static $path = null;
+	public static $max_log_size = 1000;
 	
 	protected static $colors = array(
 		1 => 31,	// error: red
@@ -74,13 +75,11 @@ class JAXLLogger {
 			$bt = debug_backtrace(); array_shift($bt); $callee = array_shift($bt);
 			$msg = basename($callee['file'], '.php').":".$callee['line']." - ".@date('Y-m-d H:i:s')." - ".$msg;
 			
-			// if no path is set via jaxl config, default log to env stderr
-			if(isset(self::$path)) {
-				error_log($msg . PHP_EOL, 3, self::$path);
-			}
-			else {
-				error_log(self::colorize($msg, $verbosity));
-			}
+			$size = strlen($msg);
+			if($size > self::$max_log_size) $msg = substr($msg, 0, self::$max_log_size) . ' ...';
+			
+			if(isset(self::$path)) error_log($msg . PHP_EOL, 3, self::$path);
+			else error_log(self::colorize($msg, $verbosity));
 		}
 	}
 	
